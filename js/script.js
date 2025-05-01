0469af7333e16594408d81bd36f24a2e296ac110
Juan Pablo Romero <romerojuanpablo.00@gmail.com>
2025-05-01 13:01:12 -0300

    css effects and contact changes
    agrego fade in visible y logos de linkedin, github y whatsapp con enlaces

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Fade-in
+    
+  // Fade-in
     const fadeEls = document.querySelectorAll('.fade-in');
+    const fadeUps = document.querySelectorAll('.fade-in-up');
+      fadeUps.forEach(el => observer.observe(el));
     const observer = new IntersectionObserver(entries => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -27,4 +30,43 @@ document.addEventListener("DOMContentLoaded", () => {
         }
       });
     });
+    //scroll to top button
+    const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 200) {
+        scrollTopBtn.style.display = "block";
+      } else {
+        scrollTopBtn.style.display = "none";
+      }
+    });
+  
+    scrollTopBtn.addEventListener("click", () => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    });
+  
   });
+
+  // Email checker button
+    const emailBtn = document.getElementById("emailBtn");
+
+  emailBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const gmailURL = "https://mail.google.com/mail/?view=cm&to=romerojuanpablo.00@gmail.com";
+    const fallbackMailto = "mailto:romerojuanpablo.00@gmail.com";
+
+    // Intentar abrir Gmail
+    const gmailWindow = window.open(gmailURL, "_blank");
+
+    // Si el navegador bloquea popup o Gmail no está disponible
+    setTimeout(() => {
+      if (!gmailWindow || gmailWindow.closed || typeof gmailWindow.closed == "undefined") {
+        window.location.href = fallbackMailto;
+      }
+    }, 1000);
+  });
+
+ 
